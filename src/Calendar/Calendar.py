f6bafef6a214ae192694c0bcfc609c023386b10e
Torsten Hahn <torsten@roost.one>
2024-04-29 20:43:57 +0200

    calendar timezone convertions

--- src/Calendar/Calendar.py
+++ src/Calendar/Calendar.py
@@ -21,9 +21,11 @@ class Calendar:
             for entry in self.entries:
                 f.write("BEGIN:VEVENT\n")
                 f.write(f"SUMMARY:{entry.title}\n")
-                # datetime objects to ical format
                 f.write(f"DTSTAMP:{datetime.now().strftime('%Y%m%dT%H%M%SZ')}\n")
-                f.write(f"DTEND:{entry.end_time}\n")
+                start_time_utc = entry.get_utc_times(entry.start_time)
+                end_time_utc = entry.get_utc_times(entry.end_time) 
+                f.write(f"DTSTART:{start_time_utc.strftime('%Y%m%dT%H%M%SZ')}\n")
+                f.write(f"DTEND:{end_time_utc.strftime('%Y%m%dT%H%M%SZ')}\n")
                 f.write(f"LOCATION:{entry.location}\n")
                 f.write("END:VEVENT\n")
             f.write("END:VCALENDAR\n")
@@ -51,22 +53,27 @@ class Calendar:
 
         # Remove the "Uhr" from the end of the string
         time_str = time_str.replace(" Uhr", "")
-        
-        print(time_str) 
 
-        start_time, end_time = time_str.replace(" ", "").split("-")
+        # Check if the time string contains a "-" and add the same time to the end if it doesn't
+        if(time_str.find("-") == -1):
+            time_str = time_str + " - " + time_str
+        start_time, end_time = time_str.replace(" ","").split("-")
         
-        # Get the start and end time as datetime objects
-        # try multiple formats "HH:MM" and "H:"
-        try:
-            start_time = datetime.strptime(start_time, "%H:%M")
-            end_time = datetime.strptime(end_time, "%H:%M")
-        except:
-            start_time = datetime.strptime(start_time, "%H")
-            end_time = datetime.strptime(end_time, "%H")
+        # add minutes to the end time if there are only hours
+        if start_time.find(":") == -1:
+            start_time = start_time + ":00"
+        if end_time.find(":") == -1:
+            end_time = end_time + ":00"
+        
+       
+        start_time = datetime.strptime(start_time, "%H:%M")
+        end_time = datetime.strptime(end_time, "%H:%M")
         
         # Combine the date with the start and end time
         start_time = datetime.combine(date, start_time.time())
         end_time = datetime.combine(date, end_time.time())
-        
+
+        # add 1 hour to the end time if it is the same as the start time
+        if start_time == end_time:
+            end_time = end_time.replace(hour=end_time.hour + 1)
         return start_time, end_time
--- src/Calendar/CalendarEntry.py
+++ src/Calendar/CalendarEntry.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 
 class CalendarEntry:
     # init method to create a new CalendarEntry object
@@ -7,4 +8,26 @@ class CalendarEntry:
         self.start_time = start_time
         self.end_time = end_time
         self.location = location
+    
+    # string representation of the object
+    def __str__(self):
+        return f"{self.title} at {self.start_time} to {self.end_time} at {self.location}"
+    
+    # get the times in utc format
+    @staticmethod
+    def get_utc_times(other_time: datetime):
+        # Define the local timezone as Europe/Berlin
+        local = pytz.timezone("Europe/Berlin")
+        
+        # Define the UTC timezone
+        utc = pytz.timezone("UTC")
+        
+        # Localize the input time to the Europe/Berlin timezone
+        local_time = local.localize(other_time)
+        
+        # Convert the local time to UTC
+        utc_time = local_time.astimezone(utc)
+        
+        # Return the time in UTC format
+        return utc_time
     
